cfd04ab6270f0b51940156ff505cf22931fa57a8
fmurat17 <fatihce23otmail.com>
2020-01-21 02:13:48 +0300

    Qeueu.cpp added.

---
+++ Queue.cpp
@@ -0,0 +1,27 @@
+										################################################
+											!!!!!!!!!!-------Queue--------!!!!!!!!!!
+										################################################
+										
+#include <queue>
+
+--> First in First out(FIFO).
+
+queue<int> q;
+
+---push(), pop()---
+
+	q.push(20);
+	q.pop();
+	
+---front(), back()---
+
+	q.front();
+	q.back();
+
+---empty()--- --> Returns boolean value.
+
+	q.empty();
+	
+---size()--- --> Returns size of the queue.
+
+	q.size();
